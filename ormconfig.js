fd9822dff5aaa5180310ba77f8db72841a9257a5
iOasys-G5-Fome-Zero <ioasysgrupo5@gmail.com>
2022-03-15 19:55:44 -0300

    refactor: ormconfig to accept heroku database connection

--- ormconfig.js
+++ ormconfig.js
@@ -17,6 +17,12 @@ module.exports = [
     logging: false,
     migrations: ['dist/infra/typeorm/migrations/*.js'],
     entities: ['dist/src/shared/entities/**/*.entity.js'],
+    ssl: true,
+    extra: {
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
     cli: {
       entitiesDir: 'src/shared/entities/',
       migrationsDir: 'infra/typeorm/migrations',
@@ -33,6 +39,12 @@ module.exports = [
     synchronize: false,
     logging: false,
     migrations: ['dist/infra/typeorm/seeds/*.js'],
+    ssl: true,
+    extra: {
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
     cli: {
       migrationsDir: 'infra/typeorm/seeds',
     },
