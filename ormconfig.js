714330b876c8b7aae93e8fa6c064c67f17c55a48
iOasys-G5-Fome-Zero <ioasysgrupo5@gmail.com>
2022-03-15 19:27:23 -0300

    refactor: refactored environments database variable

--- ormconfig.js
+++ ormconfig.js
@@ -1,19 +1,18 @@
 require('dotenv/config');
 
+const getDbData = (variable) => {
+  const local = process.env.LOCAL_MODE ? '_LOCAL' : '';
+  return process.env[variable + local];
+};
+
 module.exports = [
   {
     type: 'postgres',
-    host: process.env.DEV_MODE ? process.env.DB_HOST_DEV : process.env.DB_HOST,
-    port: process.env.DEV_MODE ? process.env.DB_PORT_DEV : process.env.DB_PORT,
-    username: process.env.DEV_MODE
-      ? process.env.DB_USERNAME_DEV
-      : process.env.DB_USERNAME,
-    password: process.env.DEV_MODE
-      ? process.env.DB_PASSWORD_DEV
-      : process.env.DB_PASSWORD,
-    database: process.env.DEV_MODE
-      ? process.env.DB_DATABASE_NAME_DEV
-      : process.env.DB_DATABASE_NAME,
+    port: getDbData('DB_PORT'),
+    host: getDbData('DB_HOST'),
+    username: getDbData('DB_USERNAME'),
+    password: getDbData('DB_PASSWORD'),
+    database: getDbData('DB_DATABASE_NAME'),
     synchronize: false,
     logging: false,
     migrations: ['dist/infra/typeorm/migrations/*.js'],
@@ -26,11 +25,11 @@ module.exports = [
   {
     name: 'seed',
     type: 'postgres',
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE_NAME,
+    port: getDbData('DB_PORT'),
+    host: getDbData('DB_HOST'),
+    username: getDbData('DB_USERNAME'),
+    password: getDbData('DB_PASSWORD'),
+    database: getDbData('DB_DATABASE_NAME'),
     synchronize: false,
     logging: false,
     migrations: ['dist/infra/typeorm/seeds/*.js'],
